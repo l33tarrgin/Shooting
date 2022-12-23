0cdec61925f867f5c7acd424721d6291da85f29a
zorkang <zorkang@mail.ru>
2022-12-23 15:12:29 +0300

    изменил тест switch_shooting_mode

--- shooting/tests/test_shooting.py
+++ shooting/tests/test_shooting.py
@@ -1,15 +1,20 @@
 from shooting import AutomaticRifle, CycleModeShooting, General
+import mock
 
 default_params = dict(name='AK-47', mag_size=4, silencer=False, bullet_speed=700)
 
-class TestGeneral:
 
-    general = General(**default_params)
+class TestAutomaticRifle:
 
-    def test_switch_shooting_mode(self):
+    gun = AutomaticRifle(**default_params)
+
+    def test_switch_shooting_mode(self, monkeypatch):
 
         # arrange, описание неизменямых параметров
+        old_shooting_mode = self.gun.shooting_mode
+        mocked_input = mock.patch('builtins.input', return_value="2")
 
         # act, вызов тестируемой функции
-
+        self.gun.switch_shooting_mode()
         # assert, сравнивание фактических результатов с ожидаемым
+        assert old_shooting_mode != self.gun.shooting_mode
