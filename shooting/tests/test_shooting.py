fa2aed6ea644f6619ae7513d62c5c17ab1f3745a
zorkang <zorkang@mail.ru>
2022-12-23 20:20:28 +0300

    Добавлено 3 теста методов класса General: test_upgrade_mag_size, test_install_silencer, test_upgrade_bullet_speed

--- shooting/tests/test_shooting.py
+++ shooting/tests/test_shooting.py
@@ -8,13 +8,49 @@ class TestAutomaticRifle:
 
     gun = AutomaticRifle(**default_params)
 
-    def test_switch_shooting_mode(self, monkeypatch):
+    def test_upgrade_mag_size(self):
+        # arrange, описание незменяемых параметров
+        old_mag_size = self.gun.mag_size
+        new_mag_size = 12
 
+        # act, вызов тестируемой функции
+        result = self.gun.upgrade_mag_size(new_mag_size)
+
+        # assert, сравнивание фактических результатов с ожидаемым
+        # assert result == new_mag_size # чому нет?
+        assert result == 'Размер магазина увеличен: текущий размер 12'
+        assert result != old_mag_size
+
+    def test_install_silencer(self):
+        # arrange, описание незменяемых параметров
+        old_install_silencer = self.gun.install_silencer
+        new_install_silencer = True
+
+        # act, вызов тестируемой функции
+        result = self.gun.install_silencer(new_install_silencer)
+
+        # assert, сравнивание фактических результатов с ожидаемым
+        assert result == "Глушитель установлен"
+        assert result != old_install_silencer
+
+    def test_upgrade_bullet_speed(self):
+        # arrange, описание незменяемых параметров
+        old_bullet_speed = self.gun.bullet_speed
+        new_bullet_speed = 1000
+
+        # act, вызов тестируемой функции
+        result = self.gun.upgrade_bullet_speed(new_bullet_speed)
+
+        # assert, сравнивание фактических результатов с ожидаемым
+        assert result == 'Скорость полета пули увеличена: текущая скорость 1000'
+        assert result != old_bullet_speed
+
+    def test_switch_shooting_mode(self, monkeypatch):
         # arrange, описание неизменямых параметров
         old_shooting_mode = self.gun.shooting_mode
-        mocked_input = mock.patch('builtins.input', return_value="2")
 
         # act, вызов тестируемой функции
         self.gun.switch_shooting_mode()
+
         # assert, сравнивание фактических результатов с ожидаемым
         assert old_shooting_mode != self.gun.shooting_mode
